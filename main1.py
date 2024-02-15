60b093a544e22b975b274dd04f9e6f8f02f1a4df
Dani722 <128698576+Dani722@users.noreply.github.com>
2024-02-15 10:44:11 -0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,8 @@
+#nome = input("Escreva seu nome")
+#idade = input("Escreva sua idade")
+#idade = int(input("Escreva sua idade"))
+#print(f"Olá, {nome}, você tem {idade} anos")
+#try:
+    #idade=int (input("Escreva sua idade"))
+#except ValueError:
+    #print("Você não digitou sua idade")
