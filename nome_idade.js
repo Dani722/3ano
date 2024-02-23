843fe8ab59087eb17e3fb08de6815da5d9fe6336
Dani722 <128698576+Dani722@users.noreply.github.com>
2024-02-23 12:00:02 -0300

    Add files via upload

---
+++ nome_idade.js
@@ -0,0 +1,7 @@
+let nome = `Daniel`
+let idade = `16`
+let altura = `175`
+let peso = `56`
+let numero = `7`
+console.log(`o seu nome é ${nome} e sua idade é ${idade} com ${altura} de altura, e também seu peso de ${peso}kg`);
+console.log(`sua idade mais o número é ${idade + numero}`);
