63017605bed709ae5f701f9db40f315274d89eef
Dani722 <128698576+Dani722@users.noreply.github.com>
2024-03-07 10:42:28 -0300

    Add files via upload

--- jogos.py
+++ jogos.py
@@ -1,3 +1,19 @@
-import.forca
-import.adivinhação
+import forca
+import advinhacao
 
+def escolhaJogo():
+    print('-------------------------------------------')
+    print('-------------Escolha o jogo----------------')
+    print('-------------------------------------------')
+    print('(1) - Forca')
+    print('(2) - Adivinhação')
+    escolha = int(input('Digite a opção desejada: '))
+    if escolha == 1:
+        forca.jogar()
+    elif escolha == 2:
+        advinhacao.jogar()
+    else:
+        print('Digite uma opção certa macaco')
+
+if(__name__=="__main__"):
+    escolhaJogo()
