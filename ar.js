b9afe914b5012991adbd7ee88a07d7e0c728d5e9
Dani722 <128698576+Dani722@users.noreply.github.com>
2024-04-15 11:31:25 -0300

    Update ar.js

--- ar.js
+++ ar.js
@@ -6,3 +6,44 @@ console.log(`o ${nome[1]} tem ${idade[1]} anos`)
 console.log(`o ${nome[2]} tem ${idade[2]} anos`)
 console.log(`o ${nome[3]} tem ${idade[3]} anos`)
 console.log(`o ${nome[4]} tem ${idade[4]} anos`)
+
+
+import 'dart:io';
+
+void main() {
+  print("Digite o primeiro número:");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  print("Digite o segundo número:");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  print("Digite a operação (+, -, *, /):");
+  String operacao = stdin.readLineSync()!;
+
+  double resultado;
+
+  switch (operacao) {
+    case '+':
+      resultado = num1 + num2;
+      break;
+    case '-':
+      resultado = num1 - num2;
+      break;
+    case '*':
+      resultado = num1 * num2;
+      break;
+    case '/':
+      if (num2 != 0) {
+        resultado = num1 / num2;
+      } else {
+        print("Erro: Divisão por zero não é permitida.");
+        return;
+      }
+      break;
+    default:
+      print("Operação inválida. Use +, -, *, ou /.");
+      return;
+  }
+
+  print("Resultado: $resultado");
+}
